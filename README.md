626f81cc4e7ded4a8478116d1f2aa07ef267fd7f
atitus5 <drewtitus59@gmail.com>
2015-12-04 14:56:44 -0500

    Basic frame set up

--- README.md
+++
@@ -1,2 +0,0 @@
-# 21G412-Collage
-Final project for Advanced German Literature and Culture (21G.412). Simple word collage with multimedia
---
+++ js/draw_collage.js
@@ -0,0 +1,4 @@
+var canvas = document.getElementById("collage-canvas");
+var ctx = canvas.getContext("2d");
+ctx.fillStyle = "#808080";
+ctx.fillRect(0,0,1000,400);
